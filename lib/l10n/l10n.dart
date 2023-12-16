1387bd8220692e911a0bc4f7ee4c2e986a808fac
minhnhatvdl <minhnhatvdl@gmail.com>
2023-12-16 19:18:07 +0100

    Feat: Add a multi lang support

---
+++ lib/l10n/l10n.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class L10n {
+  static const all = [
+    Locale('en'),
+    Locale('fr'),
+  ];
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:daily_news/features/daily_news/data/models/article_model.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:daily_news/config/router/app_router.dart';
@@ -8,8 +9,10 @@ import 'package:daily_news/features/daily_news/domain/usescases/articles_usecase
 import 'package:daily_news/features/daily_news/presentation/bloc/articles/articles_bloc.dart';
 import 'package:daily_news/features/daily_news/presentation/bloc/articles/articles_event.dart';
 import 'package:daily_news/set_up_dependencies.dart';
+import 'package:daily_news/l10n/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,6 +35,13 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         theme: AppTheme.theme,
         routerConfig: AppRouter.router,
+        localizationsDelegates: const [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: L10n.all,
       ),
     );
   }
